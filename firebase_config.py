e071866733b58e05acbda8c406cfa72cbb342698
Jose Lee <andrelee089@gmail.com>
2025-01-27 11:00:55 -0800

    added requirements.txt

--- firebase_config.py
+++ firebase_config.py
@@ -2,7 +2,7 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate('investing-controller-firebase-adminsdk-fbsvc-79d362385f.json')
+cred = credentials.Certificate('investing-controller-firebase-adminsdk-fbsvc-828509e477.json')
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
