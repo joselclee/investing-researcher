52b4b82270d6e6bbfb4843059dbd6d4d2a577d52
LevelAndre <andrelee089@gmail.com>
2025-02-05 20:15:25 -0800

    app health check endpoint

--- app.py
+++ app.py
@@ -26,3 +26,7 @@ app.register_blueprint(monte_carlo_optimization_bp)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
+    
+@app.route('/health', methods=['GET'])
+def health_check():
+    return 'OK', 200
